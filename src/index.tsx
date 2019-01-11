0b338d5ba187fe0ce782eddfa657d55b6b6e90cb
Evan Hennessy <hennessyevan@users.noreply.github.com>
2019-01-11 16:21:52 -0500

    export Box types for convenience

--- src/index.tsx
+++ src/index.tsx
@@ -1,47 +1,49 @@
-import * as cache from './cache'
-import * as styles from './styles'
+import * as cache from "./cache";
+import * as styles from "./styles";
 
-export {default} from './box'
-export {default as splitProps} from './utils/split-props'
-export {default as splitBoxProps} from './utils/split-box-props'
+export { default } from "./box";
+export { default as splitProps } from "./utils/split-props";
+export { default as splitBoxProps } from "./utils/split-box-props";
 
-export {css} from 'glamor'
+export { Box as BoxType, BoxProps } from "./box-types";
+
+export { css } from "glamor";
 export {
-  background,
-  borderRadius,
-  borders,
-  boxShadow,
-  dimensions,
-  flex,
-  interaction,
-  layout,
-  list,
-  opacity,
-  overflow,
-  position,
-  spacing,
-  text,
-  transform,
-  propTypes,
-  propNames,
-  propAliases,
-  propEnhancers
-} from './enhancers/index'
+	background,
+	borderRadius,
+	borders,
+	boxShadow,
+	dimensions,
+	flex,
+	interaction,
+	layout,
+	list,
+	opacity,
+	overflow,
+	position,
+	spacing,
+	text,
+	transform,
+	propTypes,
+	propNames,
+	propAliases,
+	propEnhancers
+} from "./enhancers/index";
 
 export function hydrate(entries) {
-  cache.hydrate(entries)
+	cache.hydrate(entries);
 }
 
 export function extractStyles() {
-  const output = {
-    cache: cache.entries(),
-    styles: styles.getAll()
-  }
-  clearStyles()
-  return output
+	const output = {
+		cache: cache.entries(),
+		styles: styles.getAll()
+	};
+	clearStyles();
+	return output;
 }
 
 export function clearStyles() {
-  cache.clear()
-  styles.clear()
+	cache.clear();
+	styles.clear();
 }
