d0d57d7bed554cd81b8e034cf1152e97329ce362
Evan Hennessy <hennessyevan@users.noreply.github.com>
2019-02-08 14:47:28 -0500

    Fix tests

--- src/expand-aliases.ts
+++ src/expand-aliases.ts
@@ -1,4 +1,4 @@
-import { propAliases, propValidators } from "./enhancers/index";
+import { propAliases, propValidators } from './enhancers/index';
 
 /**
  * Expands aliases like `margin` to `marginTop`, `marginBottom`, `marginLeft` and `marginRight`.
@@ -7,30 +7,30 @@ import { propAliases, propValidators } from "./enhancers/index";
  * properties due to the style insertion order.
  */
 export default function expandAliases(props: object) {
-	const propNames = Object.keys(props);
-	// Use a Map because it's faster for setting values and looping over than an Object
-	const newProps = new Map();
+  const propNames = Object.keys(props);
+  // Use a Map because it's faster for setting values and looping over than an Object
+  const newProps = new Map();
 
-	propNames.forEach(propName => {
-		const propValue = props[propName];
-		const aliases: string[] = propAliases[propName] || [propName];
+  propNames.forEach(propName => {
+    const propValue = props[propName];
+    const aliases: string[] = propAliases[propName] || [propName];
 
-		// Check that the alias has a valid value in development
-		if (process.env.NODE_ENV !== "production") {
-			const validator = propValidators[propName];
-			if (validator) {
-				const result = validator(propValue);
-				if (result) {
-					throw new Error(`al- aluminum-box: ${result}`);
-				}
-			}
-		}
+    // Check that the alias has a valid value in development
+    if (process.env.NODE_ENV !== 'production') {
+      const validator = propValidators[propName];
+      if (validator) {
+        const result = validator(propValue);
+        if (result) {
+          throw new Error(`al- aluminum-box: ${result}`);
+        }
+      }
+    }
 
-		// Expand aliases
-		aliases.forEach(alias => {
-			newProps.set(alias, propValue);
-		});
-	});
+    // Expand aliases
+    aliases.forEach(alias => {
+      newProps.set(alias, propValue);
+    });
+  });
 
-	return newProps;
+  return newProps;
 }
