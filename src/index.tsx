8d2d6bc86bd88268d03566e5e30134c834a4b8a7
hennessyevan <hennessyevan@users.noreply.github.com>
2019-07-01 20:49:03 -0400

    export boxprops

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,9 @@
-import * as cache from './cache'
-import * as styles from './styles'
+import * as cache from "./cache";
+import * as styles from "./styles";
 
-export {default} from './box'
-export {default as splitProps} from './utils/split-props'
-export {default as splitBoxProps} from './utils/split-box-props'
+export { default } from "./box";
+export { default as splitProps } from "./utils/split-props";
+export { default as splitBoxProps } from "./utils/split-box-props";
 
 export {
   background,
@@ -25,20 +25,22 @@ export {
   propNames,
   propAliases,
   propEnhancers
-} from './enhancers/index'
+} from "./enhancers/index";
 
-export const hydrate = cache.hydrate
+export const hydrate = cache.hydrate;
+
+export * from "./types/box-types";
 
 export function extractStyles() {
   const output = {
     cache: cache.entries(),
     styles: styles.getAll()
-  }
-  clearStyles()
-  return output
+  };
+  clearStyles();
+  return output;
 }
 
 export function clearStyles() {
-  cache.clear()
-  styles.clear()
+  cache.clear();
+  styles.clear();
 }
