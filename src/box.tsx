740a817006475a8aa9401d8c69da72f701583a12
hennessyevan <hennessyevan@users.noreply.github.com>
2019-10-17 17:22:50 -0400

    update boxprops

--- src/box.tsx
+++ src/box.tsx
@@ -1,30 +1,30 @@
-import { ClassNames, CSSObject } from "@emotion/core";
-import facepaint from "facepaint";
-import * as CSS from "csstype";
-import React, { ReactNode, useContext, forwardRef, Ref } from "react";
-import expandAliases from "./expandAliases";
-import splitCSSProps from "./splitCSSProps";
-import { MediaQueryContext } from "./mqcontext";
+import { ClassNames } from '@emotion/core'
+import { CSSInterpolation } from '@emotion/serialize'
+import * as CSS from 'csstype'
+import facepaint from 'facepaint'
+import React, { forwardRef, ReactNode, Ref, useContext } from 'react'
+import expandAliases from './expandAliases'
+import { MediaQueryContext } from './mqcontext'
+import splitCSSProps from './splitCSSProps'
 
 export interface BoxProps extends CSS.StandardPropertiesFallback<number | string | number[] | string[] | null> {
-  is?: any;
-  className?: string;
-  css?: CSSObject;
-  props?: React.InputHTMLAttributes<HTMLInputElement> & React.ClassAttributes<HTMLInputElement> | null | undefined;
-  style?: object;
-  children?: ReactNode | ReactNode[];
-  [key: string]: any;
+  is?: any
+  className?: string
+  css?: CSSInterpolation | CSSInterpolation[]
+  props?: React.InputHTMLAttributes<HTMLInputElement> & React.ClassAttributes<HTMLInputElement> | null | undefined
+  style?: object
+  children?: ReactNode | ReactNode[]
+  [key: string]: any
 }
 
-const Box = forwardRef(({ is = "div", className, innerRef, css, props = {}, style, children, ...rest }: BoxProps, ref: Ref<any>) => {
-  const mqContext = useContext(MediaQueryContext);
-  const mq = facepaint(mqContext);
-
-  [rest, css] = expandAliases(rest, css);
-  const { matched, remaining } = splitCSSProps(rest);
+const Box = forwardRef(({ is = 'div', className, innerRef, css, props = {}, style, children, ...rest }: BoxProps, ref: Ref<any>) => {
+  const mqContext = useContext(MediaQueryContext)
+  const mq = facepaint(mqContext)
+  ;[rest, css] = expandAliases(rest, css)
+  const { matched, remaining } = splitCSSProps(rest)
 
   if (innerRef) {
-    props!.ref = innerRef;
+    props!.ref = innerRef
   }
 
   return (
@@ -37,17 +37,17 @@ const Box = forwardRef(({ is = "div", className, innerRef, css, props = {}, styl
             style,
             ref,
             ...remaining,
-            ...props
+            ...props,
           },
-          children
+          children,
         )
       }
     </ClassNames>
-  );
-});
+  )
+})
 
 Box.defaultProps = {
-  boxSizing: "border-box"
-};
+  boxSizing: 'border-box',
+}
 
-export default Box;
+export default Box
